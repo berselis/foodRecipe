4c4f829b0a0c0d2e20e68f7b2f226e6d70324b0b
Berselis Mendoza <berselismendoza@gmail.com>
2022-10-27 22:45:59 -0400

    update code
    add requires to initmodel

--- project/models/initModels.js
+++ project/models/initModels.js
@@ -1,4 +1,15 @@
 const Users = require('../models/users.model');
+const Categories = require('../models/categories.model');
+const Ingredients = require('../models/ingredients.model');
+const Instructions = require('../models/instructions.model');
+const RecipesIngredients = require('../models/recipes_ingredients.model');
+const Recipes = require('../models/recipes.model');
+const Types = require('../models/types.model');
+const UsersIngredient = require('../models/users_ingredients.model');
+const UsersRecipes = require('../models/users_recipes.model');
+
+
+
 
 
 const initModels = () => {
